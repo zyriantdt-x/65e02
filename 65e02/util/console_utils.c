10bce5d75e9e2cad2eda84926b267a1ac47ffc18
Ellis <me@ellis.pw>
2023-05-13 18:49:49 +0100

    add clear console method

--- 65e02/util/console_utils.c
+++ 65e02/util/console_utils.c
@@ -1,9 +1,26 @@
 #include "console_utils.h"
 
-void console_print_at(short x, short y, const char* message) {
+void console_print_at(short x, short y, char* message) {
 	HANDLE console_handle = GetStdHandle( STD_OUTPUT_HANDLE );
 	COORD print_coords = { x,y };
 	
 	SetConsoleCursorPosition( console_handle, print_coords );
 	WriteConsoleA( console_handle, message, strlen( message ), NULL, NULL );
+}
+
+void console_clear() {
+    COORD top_left = { 0, 0 };
+    HANDLE console_handle = GetStdHandle( STD_OUTPUT_HANDLE );
+    CONSOLE_SCREEN_BUFFER_INFO screen;
+    DWORD written;
+
+    GetConsoleScreenBufferInfo( console_handle, &screen );
+    FillConsoleOutputCharacterA(
+        console_handle, ' ', screen.dwSize.X * screen.dwSize.Y, top_left, &written
+    );
+    FillConsoleOutputAttribute(
+        console_handle, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+        screen.dwSize.X * screen.dwSize.Y, top_left, &written
+    );
+    SetConsoleCursorPosition( console_handle, top_left );
 }
--- 65e02/util/console_utils.h
+++ 65e02/util/console_utils.h
@@ -2,4 +2,5 @@
 #include <Windows.h>
 #include <string.h>
 
-void console_print_at(short x, short y, const char* message);
+void console_print_at(short x, short y, char* message);
+void console_clear();
