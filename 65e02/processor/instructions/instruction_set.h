c4d9aeab3a2f272791592ace428afefec4ea2cbe
Ellis <me@ellis.pw>
2023-05-13 19:19:14 +0100

    create adc imm8 instruction

--- 65e02/processor/instructions/instruction_set.h
+++ 65e02/processor/instructions/instruction_set.h
@@ -1,4 +1,5 @@
 #pragma once
 typedef enum {
-	LDA_IMM8	= 0xA9 // load accumulator, imm8
+	LDA_IMM8	= 0xA9, // load accumulator, imm8
+	ADC_IMM8	= 0x69	// add with carry, imm8
 } Instruction;
--- 65e02/processor/processor.c
+++ 65e02/processor/processor.c
@@ -20,8 +20,10 @@ void execute_instruction( Processor* processor ) {
 
 	switch( instruction ) {
 		case LDA_IMM8: {
+			// get imm8 value
 			byte value = read_address( &( processor->address_bus ), processor->program_counter++ );
 
+			// load accumulator with value
 			processor->accumulator_register = value;
 
 			// if accumulator is set to zero, set zero flag
@@ -29,6 +31,29 @@ void execute_instruction( Processor* processor ) {
 
 			// if bit 7 is enabled, set negative flag
 			if( ( value & 0b10000000 ) > 0 ) processor->processor_status = processor->processor_status | 0b00000001;
+
+			break;
+		}
+		case ADC_IMM8: {
+			// get numbers to add
+			byte additional_value	= read_address( &( processor->address_bus ), processor->program_counter++ );
+			byte accumulator_value	= processor->accumulator_register;
+
+			// calculator sum
+			byte new_value = additional_value + accumulator_value;
+
+			// if the new value is set to zero, set zero flag
+			if( new_value == 0x00 ) processor->processor_status = processor->processor_status | 0b01000000;
+
+			// if the new value has overflowed, set the carry flag
+			if( new_value < additional_value || new_value < accumulator_value ) processor->processor_status = processor->processor_status | 0b10000000;
+
+			// if the seventh bit is set, set the negative flag
+			if( new_value & ( 1 << 6 ) ) processor->processor_status | 0b00000001;
+
+			// load accumulator with sum
+			processor->accumulator_register = new_value;
+
 			break;
 		}
 		default: {
